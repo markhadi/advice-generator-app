a84036cc831d3563867b153c5fa05b1ef87441af
markhadi <markhadi@protonmail.com>
2023-09-03 21:22:34 +0800

    animate - react spring

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,61 @@ import dividerDesktop from "./assets/images/pattern-divider-desktop.svg";
 import diceIcon from "./assets/images/icon-dice.svg";
 
 import { useState, useEffect } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 const App = () => {
   const [advice, setAdvice] = useState("Press the dice to get advice");
   const [slipId, setSlipId] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchAdvice = async () => {
+    setIsLoading(true);
     try {
       const response = await fetch("https://api.adviceslip.com/advice");
-      const data = await response.json();
-      setAdvice(data.slip.advice);
-      setSlipId(data.slip.id);
+      const { slip } = await response.json();
+      setAdvice(slip.advice);
+      setSlipId(slip.id);
     } catch (error) {
       console.error("Error fetching advice:", error);
+      setAdvice("Error fetching advice. Please try again.");
     }
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchAdvice();
   }, []);
 
+  const scaleAnimation = useSpring({
+    from: { transform: "scale(0.95)" },
+    to: { transform: "scale(1)" },
+    reset: true,
+    config: { tension: 210, friction: 50 },
+  });
+
   return (
-    <main>
-      <h1>Advice #{slipId}</h1>
-      <p>"{advice}"</p>
-      <picture>
-        <source media="(min-width: 376px)" srcSet={dividerDesktop} />
-        <img src={dividerMobile} alt="Pattern Divider" />
-      </picture>
-      <button type="button" className="btn" onClick={fetchAdvice}>
-        <img src={diceIcon} alt="Generate New Advice" />
-      </button>
-    </main>
+    <animated.main style={scaleAnimation}>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <animated.h1>Advice #{slipId}</animated.h1>
+          <animated.p>"{advice}"</animated.p>
+          <picture>
+            <source media="(min-width: 376px)" srcSet={dividerDesktop} />
+            <img src={dividerMobile} alt="Pattern Divider" />
+          </picture>
+          <button
+            type="button"
+            className="btn"
+            onClick={fetchAdvice}
+            aria-label="Generate New Advice"
+          >
+            <img src={diceIcon} alt="Generate New Advice" />
+          </button>
+        </>
+      )}
+    </animated.main>
   );
 };
 
